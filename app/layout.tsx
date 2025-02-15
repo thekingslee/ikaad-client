13a11e9e057fc0f0e281e88ac2e73acfde823f9b
Nworie Kingsley <obizzkingsley@gmail.com>
2025-02-16 00:43:41 +0100

    Mobile container height adjustment

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="bg-gray-100 items-center justify-items-center min-h-screen pb-12 sm:pb-0 p-0 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-          <div className="bg-white w-screen h-full sm:h-[694px] sm:w-[420px] border border-stone-100 grid grid-rows-[auto_1fr_auto] gap-8 p-4 sm:rounded-3xl shadow-sm">
+        <div className="bg-gray-100 items-center justify-items-center min-h-screen p-0 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+          <div className="bg-white w-screen h-screen sm:h-[694px] sm:w-[420px] border border-stone-100 grid grid-rows-[auto_1fr_auto_48px] sm:grid-rows-[auto_1fr_auto] gap-8 p-4 sm:rounded-3xl shadow-sm">
             {children}
           </div>
         </div>
